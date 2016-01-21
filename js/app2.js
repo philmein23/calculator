dd2cdcc029d57cb9bbfe4ab339f9eb3e1fc2bbee
Phil Nguyen <philnguyen@Phils-MBP.home>
2016-01-20 22:33:07 -0800

    add more functionality to js file

--- js/app2.js
+++ js/app2.js
@@ -11,11 +11,21 @@ $(function() {
             temp += value;
             $("#display").val(temp.substring(0,15));
         }
+        else if (value === "CE") {
+            temp = '';
+
+        }
+        else if (value === "C") {
+            entries = [];
+            temp = '';
+
+        }
         else if (value === "X") {
             entries.push(temp);
             entries.push("*");
             temp = '';
         }
+
         else if (value === "/") {
             entries.push(temp);
             entries.push("/");
@@ -31,7 +41,10 @@ $(function() {
             entries.push("+");
             temp = "";
         }
+        el
         else if (value === "=") {
+
+            entries.push(temp);
 
             var num = Number(entries[0]);
 
@@ -40,16 +53,23 @@ $(function() {
                 var symbol = entries[i];
 
 
-                if (symbol === "*") {num *= otherNum}
-                else if (symbol === "/") {num /= otherNum}
-                else if (symbol === "+") {num += otherNum}
-                else if (symbol === "-") {num -= otherNum}
+                if (symbol === "*") {num *= otherNum;}
+                else if (symbol === "/") {num /= otherNum;}
+                else if (symbol === "+") {num += otherNum;}
+                else if (symbol === "-") {num -= otherNum;}
 
 
-                $("#display").val()
 
 
             }
+
+            $("#display").val(num);
+            entries = [];
+            temp ='';
+
+
+
+
         }
 
 });
